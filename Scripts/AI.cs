98a973ea6e01e7f0a646cfe14023557310465f8f
JonGrayson <49501559+JonGrayson@users.noreply.github.com>
2020-01-13 20:51:49 -0700

    Add files via upload

---
+++ Scripts/AI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    public Transform target;
+
+    public Rigidbody rb;
+
+    public float moveSpeed = 2000f;
+
+    public bool AIstate = true;
+
+    Vector3 direction;
+
+    Vector3 moveDirection;
+
+    void Update()
+    {
+        if(AIstate == true)
+        {
+            Seek();
+        }
+        if(AIstate == false)
+        {
+            Flee();
+        }
+    }
+
+    void Seek()
+    {
+        direction = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(direction);
+        moveDirection = direction.normalized * moveSpeed * Time.deltaTime;
+        transform.LookAt(moveDirection);
+        rb.AddForce(moveDirection);
+    }
+
+    void Flee()
+    {
+        direction = transform.position - target.position;
+        transform.rotation = Quaternion.LookRotation(direction);
+        moveDirection = direction.normalized * moveSpeed * Time.deltaTime;
+        transform.LookAt(moveDirection);
+        rb.AddForce(moveDirection);
+    }
+}
